eda1a4980fcff0b5519eba19f8b1bf49356867b7
Romanxt66 <nicolastorresrr@gmail.com>
2026-03-06 11:34:09 -0500

    Update 03_json.py

--- 03_json.py
+++ 03_json.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, Response
 
 app = Flask(__name__)
 
-# Base de datos simulada en memoria (Lista de Diccionarios)
+
 sensores_trapiche: list[dict] = [
     {"id": "S1", "tipo": "Temperatura", "valor": 95.5, "estado": "Activo"},
     {"id": "S2", "tipo": "Presion", "valor": 40.2, "estado": "Alerta"}
@@ -21,10 +21,10 @@ def buscar_sensor(id_sensor: str) -> tuple[Response, int]:
     """Búsqueda específica. Retorna el objeto y un Código de Estado HTTP."""
     for sensor in sensores_trapiche:
         if sensor["id"] == id_sensor:
-            return jsonify(sensor), 200 # 200 OK
+            return jsonify(sensor), 200 
 
-    # Arquitectura resiliente: 404 (Not Found)
+
     return jsonify({"error": "Sensor no encontrado"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True
